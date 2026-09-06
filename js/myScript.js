f6f706ca32457e97aeec60c7d0cde582249057f0
ramiroseco <rseco17@gmail.com>
2026-09-05 21:07:15 -0300

    Avance en los ejercicios 2 y 3

--- js/myScript.js
+++ js/myScript.js
@@ -33,3 +33,41 @@ function verificarDni(){
     }
 }
 
+//Ejercicio 3 Gestión de actividades deportivas
+
+function Actividad(nombre,lugar,dia,horario,cupo,estado){
+    this.nombre = nombre;
+    this.lugar = lugar;
+    this.dia = dia;
+    this.horario = horario;
+    this.cupo = cupo;
+    this.estado = estado;
+
+}
+let Futbol = new Actividad("Futbol","Cancha de futbol","Miércoles","19:00 - 21:00","10","disponible");
+let Basquet = new Actividad("Basquet","Playón Polideportivo","Miércoles","16:00","9","disponible");
+let Voley = new Actividad("Voley","Playón Polideportivo","Viernes","17:00","10","disponible");
+let Atletismo = new Actividad("Atletismo","Pista de atletismo","Sábado","09:00","10","disponible");
+console.log(Futbol);
+console.log(Basquet);
+console.log(Voley);
+console.log(Atletismo);
+
+
+class SistemaDeportes{
+        constructor(actividades) {
+
+            this.actividades = actividades;
+        }
+
+        agregarActividades = function(nuevasActividades){
+            actividades.push(nuevasActividades);
+        }
+        listarActividades = function(){
+
+        }
+
+}
+
+let SistemaDeportes1 = new SistemaDeportes([Futbol,Basquet,Voley,Atletismo]);
+console.log(SistemaDeportes1.actividades.length);
